ef2d0bb9d71d3deb5bb4dd61dc3619ed3281ca5e
YanQianyu <284989344@qq.com>
2020-03-14 22:19:22 +0800

    modify router logic and build file

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     proxyTable: {
       '/api': {
         target: 'http://localhost:8080',
