f13abfe6da29903b5ae38c6aa1d8d2d63522b5a9
SE18302010034 <07f40e457b00263f1f58c00a251b6174@huawei.com>
2020-03-24 19:45:17 +0800

    hzh

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     proxyTable: {
       '/api': {
         target: 'http://localhost:8080',
@@ -57,7 +57,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
